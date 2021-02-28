61f886d6d54e6c55d6e00f3f0838836096ebd71e
Gerardo Lijs <gerardo@micropticsl.com>
2021-02-28 06:47:48 +0100

    initial required code based on NETworkManager project

---
+++ SingleInstance_WPF/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SingleInstance_WPF
+{
+    public partial class App : Application
+    {
+        // Single instance identifier
+        private const string ApplicationSingleInstanceGuid = "{910e8c27-ab31-4043-9c5d-1382707e6c93}";
+        private static System.Threading.Mutex _singleInstanceMutex;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Create mutex
+            _singleInstanceMutex = new System.Threading.Mutex(true, ApplicationSingleInstanceGuid);
+            var mutexIsAcquired = _singleInstanceMutex.WaitOne(TimeSpan.Zero, true);
+
+            // TODO: Read from settings if you want your user to be able to opt-in to multiple instance or not
+            var settings_AllowMultipleInstances = false;
+            if (settings_AllowMultipleInstances || mutexIsAcquired)
+            {
+                // Show main window
+                StartupUri = new Uri("MainWindow.xaml", UriKind.Relative);
+
+                // Release mutex
+                _singleInstanceMutex.ReleaseMutex();
+            }
+            else
+            {
+                // Bring the already running application into the foreground
+                SingleInstance.PostMessage((IntPtr)SingleInstance.HWND_BROADCAST, SingleInstance.WM_SHOWME, IntPtr.Zero, IntPtr.Zero);
+                Shutdown();
+            }
+        }
+    }
+}
---
+++ SingleInstance_WPF/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SingleInstance_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Handle WndProc messages (Single Instance)
+        private System.Windows.Interop.HwndSource _hwndSoure;
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            _hwndSoure = System.Windows.Interop.HwndSource.FromHwnd(new System.Windows.Interop.WindowInteropHelper(this).Handle);
+            _hwndSoure?.AddHook(HwndHook);
+        }
+
+        [DebuggerStepThrough]
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            // Single instance --> Show window
+            if (msg == SingleInstance.WM_SHOWME)
+            {
+                BringWindowToFront();
+                handled = true;
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private void BringWindowToFront()
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                WindowState = WindowState.Normal;
+            }
+
+            Activate();
+        }
+        #endregion
+    }
+}
